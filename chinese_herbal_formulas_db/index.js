a7538a0ed1314ed596b42a16b427ae60eb692d27
Mona Temchin <monatemchin@gmail.com>
2018-01-18 10:58:31 -0500

    Added crud page, etc.

--- chinese_herbal_formulas_db/index.js
+++ chinese_herbal_formulas_db/index.js
@@ -5,17 +5,22 @@ const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
+
+const morgan = require('morgan');
+const PORT = 2000;
+
+app.set('view engine', 'ejs');
+
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
 
 const formulas = require('./routes/formulas');
-
 
 app.use(formulas);
 
 
-
-app.listen(2000, function() {
+app.listen(Port, function() {
   console.log("Listening on port 2000");
 });
--- chinese_herbal_formulas_db/routes/formulas.js
+++ chinese_herbal_formulas_db/routes/formulas.js
@@ -18,15 +18,39 @@ router.get('/formulas/:fid', (req, res) => {
     res.json(data);
   });
 });
+const FORMULAS_URL = 'http://localhost:2000/formulas';
+/*
 
+newForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // PREVENT DEFAULT ON FORM
+
+  axios.post(`BIRDS_URL/add`, {
+    title: newForm.querySelector('#title').value,
+    description: newForm.querySelector('#description').value
+  }).then((response) => {
+      const bird = response.data;
+      const birdEl = document.createElement('div');
+
+      birdEl.innerHTML = `<div>
+       <h2>  ${bird.title} </h2>
+        <p>  ${bird.description} </p>
+     </div>`;
+
+      birds.append(birdEl);
+
+      newForm.reset();
+    });
+
+*/
+/*
 router.post('/formulas', (req, res) => {
   queries.formulas.create(req.body)
   .then(data => {
     res.json(data[0]);
   });
 });
-
-router.put('/formulas/:fid', (req, res) => {
+*/
+router.put('/formulas/update/:fid', (req, res) => {
   let fid = req.params.fid;
   let english_name = req.params.english_name;
   let formula = req.body;
@@ -36,7 +60,7 @@ router.put('/formulas/:fid', (req, res) => {
   });
 });
 
-router.delete('/formulas/:fid', (req, res) => {
+router.delete('/formulas/delete/:fid', (req, res) => {
   queries.formulas.delete(req.params.fid)
   .then(() => {
     res.json({
